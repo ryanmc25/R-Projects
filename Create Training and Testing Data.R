f2b1c2e285d79f9a13b9b35e05f5b2547d1e16d2
ryanmc25 <85520712+ryanmc25@users.noreply.github.com>
2021-08-16 11:45:45 -0400

    Add files via upload

---
+++ Create Training and Testing Data.R
@@ -0,0 +1,35 @@
+
+# original function used to sub-divide dataframes into training and testing sets for model testing
+ # proportion of data used for training dataframe controlled by train.size argument (defaults to 70%)
+ # training dataframe contains 70% of input dataframe's non-missing data (train)
+ # testing dataframe contains 30% of input dataframe's non-missing data (test)
+ # output is a list of the 2 above-mentioned dataframes (set)
+
+train_test <- function(data, train.size=0.7) {
+  
+  dt <- sample(x=nrow(data), size=nrow(data)* train.size)
+data_nm <- na.omit(data)
+train<-data_nm[dt,]
+test<- data_nm[-dt,]
+set <- list(train, test)
+names(set) <- c("train", "test")
+return(set)
+}
+
+# using function on publicly available data
+
+# url of data set 
+url <- "http://peopleanalytics-regression-book.org/data/salespeople.csv"
+
+# load the data set and store it as a dataframe called salespeople
+
+salespeople <- read.csv(url)
+
+
+set1 <- train_test(salespeople)
+
+set2 <- train_test(salespeople, train.size = 0.5)
+
+
+
+ 
